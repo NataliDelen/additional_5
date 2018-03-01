93ed944bd8c083e8cd66fd10179392348f6143b4
Делендик Наталия Сергеевна <dns3@PrisNet.local>
2018-03-01 09:50:52 +0300

    feature/additional_5/add correct name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	
+	var length = str.length;
+	var arr = ['(', ')'];
+	
+	if( length % 2 !== 0) return false;
+	
+	var half = length / 2;
+	
+	var str1 = str.slice(0,(half+1));
+	var str2 = str.slice(half);
+	
+	
+	
+	while(str1[half] == str2[0] && half != 0){
+	  str1 = str1.slice(0,half);
+	  str2 = str2.slice(1);
+	  half--;
+  }
+  
+  if (half == 0) return true;
+  if (half != 0) return false;
 }
